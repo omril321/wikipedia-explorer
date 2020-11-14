0beca48929afee1ac666e49c468e4341c48c5d77
Omri Lavi <omri@testim.io>
2020-11-14 22:36:34 +0200

    refactor, use 'async' loading

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import './App.scss';
 import * as React from 'react';
 import { useState } from 'react';
 import classnames from 'classnames';
-import { useFileLoader } from './useFileLoader';
+import { FOLDER_WITH_METADATA, useFileLoader } from './useFileLoader';
 
 type NAV_DIRECTION = 'NONE' | 'FORWARD' | 'BACKWARD';
 
-const EMPTY_PLACEHOLDER = {content: [], path: []};
+const EMPTY_PLACEHOLDER: FOLDER_WITH_METADATA = { path: [], folderFileListing: { state: 'loaded', value: [] } };
 
 const DIRECTION_TO_CLASS = {
   'NONE': undefined,
@@ -16,11 +16,18 @@ const DIRECTION_TO_CLASS = {
 //TODO: use a "loading" for each directory
 
 
-const FolderView = ({ onFileClicked, folder }: { onFileClicked: (string: string) => void, folder: string[] }) => {
+const FolderView = ({ onFileClicked, folder }: { onFileClicked: (string: string) => void, folder: FOLDER_WITH_METADATA }) => {
+  if(folder.folderFileListing.state === 'failed') {
+    return <div> FAILED </div>
+  }
+  if(folder.folderFileListing.state === 'loading') {
+    return <div> LOADING </div>
+  }
+
   return (
     <div className="folder-view">
       {
-        folder.map((dir, index) => (
+        folder.folderFileListing.value.map((dir, index) => (
           <button className="folder-item" key={index} onClick={() => onFileClicked(dir)}>{dir}</button>
         ))
       }
@@ -30,7 +37,6 @@ const FolderView = ({ onFileClicked, folder }: { onFileClicked: (string: string)
 
 
 const Navigator = () => {
-  //TODO: organize the directories in a linked-list or something similar
   const { currentFolder, navigateBackFromCurrentFolder, longestNavigationStack, navigateToFolder } = useFileLoader();
 
   const navigationStackWithPlaceholder = [...longestNavigationStack, EMPTY_PLACEHOLDER]; //TODO: can this be better..?
@@ -65,10 +71,11 @@ const Navigator = () => {
       <div>
         <button onClick={onBackClicked}>BACK</button>
       </div>
+      <div>PATH: {currentFolder.path.join(' -> ')}</div>
       <div className={classnames("folders-container", DIRECTION_TO_CLASS[direction])}>
         {navigationStackWithPlaceholder.map((folder, index) => (
           <div className={folderClassAtIndex(index)}>
-            <FolderView key={index} folder={folder.content} onFileClicked={onFolderClicked} />
+            <FolderView key={index} folder={folder} onFileClicked={onFolderClicked} />
           </div>
         ))}
       </div>
--- src/useFileLoader.ts
+++ src/useFileLoader.ts
@@ -13,41 +13,50 @@ const DIR10 = 'dir10';
 
 const ROOT = '___root___';
 
-type FOLDER_CONTENT = string[];
+export interface FOLDER_WITH_METADATA {
+    path: string[],
+    folderFileListing:
+        {state: 'loading'} |
+        {state: 'failed'} |
+        {state: 'loaded', value: string[]},
+}
 
-const folderContents = {
+
+const staticFileContent = { //TODO: load this from wikipedia
     [ROOT]: [DIR1],
     [DIR1]: [DIR2, DIR4],
     [DIR2]: [DIR3, DIR7],
     [DIR3]: [DIR8, DIR9, DIR10],
     [DIR4]: [DIR6, DIR5],
-} as { [key: string]: FOLDER_CONTENT };
+} as { [key: string]: string[] };
 
-interface FOLDER_WITH_METADATA {
-    path: string[],
-    content: FOLDER_CONTENT,
+function loadedStaticFolderAtPath(fullPath: string[]): FOLDER_WITH_METADATA {
+    const folderKey = fullPath[fullPath.length - 1];
+    return { path: fullPath, folderFileListing: { state: 'loaded', value: staticFileContent[folderKey] } };
 }
 
 export const useFileLoader = () => {
-    const [currentFolder, setCurrentFolder] = useState<FOLDER_WITH_METADATA>({path: [ROOT], content: folderContents[ROOT]});
-    const [longestNavigationStack, setLongestNavigationStack] = useState<FOLDER_WITH_METADATA[]>([currentFolder]); //TODO: this data duplication is proably redundant
+    const [currentFolder, setCurrentFolder] = useState<FOLDER_WITH_METADATA>(loadedStaticFolderAtPath([ROOT]));
+    const [longestNavigationStack, setLongestNavigationStack] = useState<FOLDER_WITH_METADATA[]>([currentFolder]);
 
     const navigateToFolder = (folder: string) => {
-        // const newPath = [...currentFolder.path, folder];
-        const newCurrentFolder = { path: [...currentFolder.path, folder], content: folderContents[folder] };
+        if(currentFolder.folderFileListing.state !== 'loaded') {
+            //TODO: handle
+            throw new Error()
+        }
+        const newCurrentFolder = loadedStaticFolderAtPath([...currentFolder.path, folder]);
         setCurrentFolder(newCurrentFolder);
-        setLongestNavigationStack([...longestNavigationStack, newCurrentFolder]);
+        const stack = newCurrentFolder.path.map((_, index) => loadedStaticFolderAtPath(newCurrentFolder.path.slice(0, index + 1)));
+        setLongestNavigationStack(stack);
     }
 
     const navigateBackFromCurrentFolder = () => {
         setCurrentFolder(curr => {
             const newPath = [...curr.path].reverse().slice(1).reverse();
-            const newCurrentFolder = newPath[newPath.length - 1];
-            const newContent = folderContents[newCurrentFolder];
-            return {content: newContent, path: newPath};
+            return loadedStaticFolderAtPath(newPath);
         });
     }
 
-    return {currentFolder, navigateBackFromCurrentFolder, longestNavigationStack, navigateToFolder}
+    return { currentFolder, longestNavigationStack, navigateToFolder, navigateBackFromCurrentFolder };
 }
 
